ef701a7c531eb835aede6423ef02347bd31692f9
MorphewPrime <54487351+MorphewPrime@users.noreply.github.com>
2022-04-10 02:39:34 -0500

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,14 @@ window.onload = function(){
     hex = Math.floor(Math.random()*16777215).toString(16);
     $("body").css("background-color",`#${hex}`);
     $("h1").css("color",`#${hex}`);
+
+    for (let j = 2; j <= 5; j++) {
+        for (let i = 0; i <= 6; i++) {
+            $(`input[name=r${j}c${i}]`).prop( "disabled", true );
+        }
+    }
+    
+
     
 
 }; 
@@ -13,6 +21,10 @@ $(document).keyup(function(event) {
     if (event.which === 13) {
         // check if its the last letter if so check and submit
         const row = event.target.name.charAt(1);
+
+        for (let m = 0; m <= 6; m++) {
+            $(`input[name=r${parseInt(row) + 1}c${m}]`).prop( "disabled", false );
+        }
         // const col = event.target.name.charAt(3);
         let valid = true;
         let input = "";
@@ -42,7 +54,7 @@ $(document).keyup(function(event) {
         }
 
         // move to next line and do shiz. 
-        if (row != 6) {
+        if (row != 5) {
             document.getElementsByName(`r${parseInt(row) + 1}c1`)[0].focus();          
             // TODO: shiz
         }
@@ -57,7 +69,7 @@ $(document).keyup(function(event) {
             if (hex.toLowerCase().charAt(i - 1) === input.toLowerCase()[p]) { // green!
                 greenCount++;
                 document.getElementsByName(name)[0].style.backgroundColor = '#44aa5c';
-            } else if (hex.toLowerCase().includes(input.toLowerCase()   .charAt(i-1), 0)) { // yellow!
+            } else if (hex.toLowerCase().includes(input.toLowerCase().charAt(i-1), 0)) { // yellow!
                 document.getElementsByName(name)[0].style.backgroundColor = '#fceea7';
             } else { // RED!!
                 document.getElementsByName(name)[0].style.backgroundColor = '#AA4A44';
@@ -73,7 +85,11 @@ $(document).keyup(function(event) {
             
             // elem
 
-            // $(`input[name=${`r${row + 1}c${i}`}]`).attr("disabled", "disabled");
+            // $(`input[name=${`r${row + 1}c${i}`}]`).prop( "disabled", true );
+            console.log("yooo", `input[name=r${row+1}c${i}]`);
+            $(`input[name=r${parseInt(row)}c${i}]`).prop( "disabled", true );
+            
+            // $(`input[name=${`r3c1`}]`).prop( "disabled", true );
             document.getElementsByName(name).readOnly = true;
 
         }
@@ -81,6 +97,8 @@ $(document).keyup(function(event) {
         if (greenCount == 6) { // weiner weiner ckn dinnner
             alert("YOU ARE AWESOME! And a Weiner");
             // lockFollowingRows(row);
+        } else if (row == 5) {
+            alert(`You lose, Hex value was ${hex}`);
         }
         console.log(event.target.name);
         // alert('Enter is pressed!');
